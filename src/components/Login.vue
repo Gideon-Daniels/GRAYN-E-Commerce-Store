341acfa808a9b76cce01868fb24461edda6ede2e
Gideon-Daniels <gideondaniels.dragoonix@gmail.com>
2021-10-27 15:02:25 +0200

    update

---
+++ src/components/Login.vue
@@ -0,0 +1,19 @@
+<template>
+<div class="modal-container">
+  <form>
+    
+    <input type="Email" required placeholder="Email">
+  </form>
+
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
