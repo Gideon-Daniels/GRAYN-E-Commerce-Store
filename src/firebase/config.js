625e4a82a3e2dc82f2bd4eb98e0792dc3a335302
retsepile <Retshepilekoloko27@gmail.com>
2021-10-29 14:48:12 +0200

    Created the signup

---
+++ src/firebase/config.js
@@ -0,0 +1,26 @@
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/firestore'
+import 'firebase/compat/auth'
+import 'firebase/compat/storage'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDGQv0FYP55WizUkTB6PS7Q8FKGXM1flw4",
+    authDomain: "grayn-e-commerce-store-5e2b8.firebaseapp.com",
+    projectId: "grayn-e-commerce-store-5e2b8",
+    storageBucket: "grayn-e-commerce-store-5e2b8.appspot.com",
+    messagingSenderId: "677019520746",
+    appId: "1:677019520746:web:88d4e14c7a9c2536ad9193"
+  };
+
+// init firebase
+firebase.initializeApp(firebaseConfig)
+
+// init services
+const projectFirestore = firebase.firestore()
+const projectAuth = firebase.auth()
+const projectStorage = firebase.storage()
+
+// timestamp
+const timestamp = firebase.firestore.FieldValue.serverTimestamp
+
+export { projectFirestore, projectAuth, projectStorage, timestamp, }
--- src/views/Registration.vue
+++ src/views/Registration.vue
@@ -1,69 +1,64 @@
 <template>
-<div class="container">
-  <div class="row">
-    <div class="col">
-      <form class="row g-3 needs-validation" novalidate>
-  <div class="col-md-4">
-    <label for="validationCustom01" class="form-label" >First name</label>
-    <input type="text" class="form-control" id="validationCustom01" value="" required>
-    <div class="valid-feedback">
-        Looks good  
-    </div>
-  </div>
-  <div class="col-md-4">
-    <label for="validationCustom02" class="form-label">Last name</label>
-    <input type="text" class="form-control" id="validationCustom02" value="" required>
-    <div class="valid-feedback">
-      Looks good!
-    </div>
-  </div>
-  <div class="col-md-4">
-    <label for="validationCustomUsername" class="form-label">Email</label>
-    <div class="input-group has-validation">
-      <input type="email" class="form-control" id="validationCustomUsername" aria-describedby="inputGroupPrepend" required>
-      <div class="invalid-feedback">
-        Please enter a vaild email.
-      </div>
-    </div>
-  </div>
-  <div class="col-md-4">
-    <label for="validationCustomUsername" class="form-label">Password</label>
-    <div class="input-group has-validation">
-      <input type="password" class="form-control" id="validationCustomUsername" aria-describedby="inputGroupPrepend" required>
-      <div class="invalid-feedback">
-        Please enter at least 6 words/numbers.
-      </div>
-    </div>
-  </div>
-  <div class="col-12">
-    <div class="form-check">
-      <input class="form-check-input" type="checkbox" value="" id="invalidCheck" required>
-      <label class="form-check-label" for="invalidCheck">
-        Agree to terms and conditions
-      </label>
-      <div class="invalid-feedback">
-        You must agree before submitting.
-      </div>
-    </div>
-  </div>
-  <div class="col-12">
-    <button class="btn btn-primary" type="submit">Submit form</button>
-  </div>
-</form>
-    </div>
-    <div class="col">
-      <p>Don't miss out get the deals while they still hot </p>
-    </div>
-  </div>
-</div>
+  <form @submit.prevent="handleSubmit">
+    <input type="text" required v-model="name" placeholder="name">
+    <input type="text" required v-model="lastName" placeholder="surname">
+    <input type="email" required v-model="email" placeholder="email">
+    <input type="password" required v-model="password" placeholder="password">
+    <input type="tel" required v-model="number" placeholder="+27 555 8789">
+    <button>Sign up</button>
+  </form>
 </template>
 
 <script>
+import { ref } from 'vue'
+import useSignup from '../composables/useSignup'
 export default {
+  setup() {
+    const { error, signup} = useSignup()
+
+    // refs
+    const name = ref ('')
+    const lastName = ref('')
+    const email = ref ('')
+    const password = ref('')
+    const number = ref('')
+
+    const handleSubmit = async () => {
+      await signup(email.value, password.value, name.value)
+      console.log('user signed up')
+    }
+    return{ name,lastName, email, password, number, handleSubmit }
+  }
 
 }
 </script>
 
 <style>
+form{
+    text-align: center;
+    padding: 20px 0;
+    box-sizing: border-box;
+}
+input{
+    width: 100%;
+  padding: 15px;
+  margin: 5px 0 22px 0;
+  display: inline-block;
+  border: none;
+  background: #f1f1f1;
+  background-color: #ddd;
+  outline: none;
+    
 
+}
+button{
+    background-color: #04AA6D;
+  color: white;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  cursor: pointer;
+  width: 50%;
+  opacity: 0.9;
+}
 </style>
