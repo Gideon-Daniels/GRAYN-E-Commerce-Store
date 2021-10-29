a274b8dc143302b445ce576d83f8bc1ec68baf49
Abdul-Malik <82204978+abdul-malik360@users.noreply.github.com>
2021-10-29 15:21:24 +0200

    navbar changes and created footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,4 +1,27 @@
-<template></template>
+<template>
+  <div class="container">
+    <footer class="py-3 my-4">
+      <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+        <li class="nav-item">
+          <a href="#" class="nav-link px-2 text-muted"
+            ><i class="fab fa-twitter"></i
+          ></a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link px-2 text-muted"
+            ><i class="fab fa-instagram"></i
+          ></a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link px-2 text-muted"
+            ><i class="fab fa-facebook"></i
+          ></a>
+        </li>
+      </ul>
+      <p class="text-center text-muted">&copy; 2021 GRAYN, Inc</p>
+    </footer>
+  </div>
+</template>
 
 <script>
 export default {};
