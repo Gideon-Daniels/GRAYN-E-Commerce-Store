521686ee82b52b4ca707f417039e5c805909a774
yusalie <yusuf.salie96@gmail.com>
2021-11-04 10:22:24 +0200

    update-add to cart

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -61,7 +61,9 @@
         <button @click="handleClick('logout')" type="button" class="btn btn-outline-primary me-2">
           Logout
         </button>
-        <button @click="handleClick('cart')" type="button" class="btn btn-primary">Cart</button>
+        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
+  <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+</svg> Cart </button>
       </div>
 
       <div v-else class="col-sm-3 text-end">
@@ -70,7 +72,25 @@
         </button>
         <button @click="handleClick('signup')" type="button" class="btn btn-primary">Signup</button>
       </div>
-
+      <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Cart</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <div class="cart-items">
+          
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Continue shopping</button>
+        <button type="button" @click="handleClick('cart')" class="btn btn-primary" data-bs-dismiss="modal">Checkout</button>
+      </div>
+    </div>
+  </div>
+</div>
       
     </header>
 </template>
