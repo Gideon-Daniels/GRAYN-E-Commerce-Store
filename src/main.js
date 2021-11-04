36b1d7e65802d419bac5cbd5bac14bb0b481ee56
Gideon-Daniels <gideondaniels.dragoonix@gmail.com>
2021-11-04 12:31:45 +0200

    update-merged with Yusuf

--- src/main.js
+++ src/main.js
@@ -3,8 +3,8 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "bootstrap/dist/css/bootstrap.min.css";
-// import "bootstrap/dist/js/bootstrap.bundle";
-// import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "bootstrap/dist/js/bootstrap.bundle";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "bootstrap/js/dist/carousel"
 import { projectAuth } from "./firebase/config";
 // fires when firebase detects a change in authentication
