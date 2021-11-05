b142de3a10d933b8443778ee7238789c8fd57177
Gideon-Daniels <gideondaniels.dragoonix@gmail.com>
2021-11-05 09:10:14 +0200

    update

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -4,15 +4,26 @@
     <div v-if="error" class="error">Could not fetch the data</div>
     <!-- container -->
     <div v-if="documents" class="container">
-      <div v-for="doc in documents" :key="doc.id">
-        {{ doc.title }}
+      <div class="row">
+
+      <div class="card" style="width: 18rem;" v-for="doc in documents" :key="doc.id" >
+        <img :src="doc.coverUrl" class="card-img-top" alt="">
+        <div class="card-body">
+          <h5 class="card-title">{{doc.title}}</h5>
+          <p class="card-text">{{doc.description}}</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
       </div>
     </div>
+
   </section>
+
 </template>
 
 <script>
 import getCollection from "../composables/getCollection";
+
 export default {
   setup() {
     const { error, documents } = getCollection("products");
