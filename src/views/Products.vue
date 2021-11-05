fe8870a8a8c358fca1964952b70e4103f162334b
Gideon-Daniels <gideondaniels.dragoonix@gmail.com>
2021-11-05 16:23:18 +0200

    update

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -3,26 +3,20 @@
     <!-- error message -->
     <div v-if="error" class="error">Could not fetch the data</div>
     <!-- container -->
-    <div v-if="documents" class="">
-      <!-- search bar -->
-      <div class="row">
-        <h1>search bar</h1>
+    <div v-if="products" class="row">
+      <div class="col-2">
+        <button @click="filterCategories('Shoes')">Shoes</button>
+        <button @click="filterCategories('Tops')">Tops</button>
+        <button @click="filterCategories('Tops')">Shoes</button>
+        <button @click="filterCategories('Tops')">Shoes</button>
       </div>
-      <!-- cards -->
-      <div class="row d-flex justify-content-center">
-        <div
-          class="card"
-          style="width: 18rem;"
-          v-for="doc in documents"
-          :key="doc.id"
-        >
-          <img :src="doc.coverUrl" class="card-img-top" alt="" />
-          <div class="card-body">
-            <h5 class="card-title">{{ doc.title }}</h5>
-            <p class="card-text">{{ doc.description }}</p>
-            <a href="#" class="btn btn-primary">Add To Cart</a>
-          </div>
+      <div class="col-10">
+        <!-- search bar -->
+        <div class="row mt-2 mb-2">
+          <SearchBar />
         </div>
+        <!-- cards -->
+        <ListProducts :products="products" />
       </div>
     </div>
   </section>
@@ -30,23 +24,32 @@
 
 <script>
 import getCollection from "../composables/getCollection";
-
+import SearchBar from "../components/search.vue";
+import ListProducts from "../components/ListProducts.vue";
 export default {
+  components: { SearchBar, ListProducts },
   setup() {
-    const { error, documents } = getCollection("products");
+    var { error, documents: products } = getCollection("products");
 
-    return { error, documents };
+    const filterCategories = async (category) => {
+      // console.log(category);
+      products = await getCollection("products", ["categorys", "==", category]);
+      console.log("products: ", products);
+    };
+
+    // console.log(products);
+    return { error, products, filterCategories };
   },
 };
 </script>
 
 <style scoped>
-  img{
-    width: 100%;
-    height:  100%;
-    object-fit: cover;
-  }
-  .products{
-    overflow: hidden;
-  }
+img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+.products {
+  overflow: hidden;
+}
 </style>
