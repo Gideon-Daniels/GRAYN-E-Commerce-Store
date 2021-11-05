69ca05ee2b9a601303fba5b927285784f3360c84
Gideon-Daniels <gideondaniels.dragoonix@gmail.com>
2021-11-05 10:27:16 +0200

    update

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,24 +1,31 @@
 <template>
-  <section class="Products">
+  <section class="products">
     <!-- error message -->
     <div v-if="error" class="error">Could not fetch the data</div>
     <!-- container -->
-    <div v-if="documents" class="container">
+    <div v-if="documents" class="">
+      <!-- search bar -->
       <div class="row">
-
-      <div class="card" style="width: 18rem;" v-for="doc in documents" :key="doc.id" >
-        <img :src="doc.coverUrl" class="card-img-top" alt="">
-        <div class="card-body">
-          <h5 class="card-title">{{doc.title}}</h5>
-          <p class="card-text">{{doc.description}}</p>
-          <a href="#" class="btn btn-primary">Add To Cart</a>
+        <h1>search bar</h1>
+      </div>
+      <!-- cards -->
+      <div class="row d-flex justify-content-center">
+        <div
+          class="card"
+          style="width: 18rem;"
+          v-for="doc in documents"
+          :key="doc.id"
+        >
+          <img :src="doc.coverUrl" class="card-img-top" alt="" />
+          <div class="card-body">
+            <h5 class="card-title">{{ doc.title }}</h5>
+            <p class="card-text">{{ doc.description }}</p>
+            <a href="#" class="btn btn-primary">Add To Cart</a>
+          </div>
         </div>
       </div>
-      </div>
     </div>
-
   </section>
-
 </template>
 
 <script>
@@ -33,4 +40,13 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+  img{
+    width: 100%;
+    height:  100%;
+    object-fit: cover;
+  }
+  .products{
+    overflow: hidden;
+  }
+</style>
